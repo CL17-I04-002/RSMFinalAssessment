e2df18c8a4397332cfc787fdb58d77bcc4f918c9
DESKTOP-9BD52QQ\LARIN <daniel_larin77@outlook.com>
2025-05-02 21:12:35 -0600

    Google Maps Integration

--- NorthwindTraders/obj/Debug/net8.0/NorthwindTraders.AssemblyInfo.cs
+++ NorthwindTraders/obj/Debug/net8.0/NorthwindTraders.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindTraders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fd660d2814753e872691a1293f7a2e3af1114c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa419706471c078f0e5f862ec272437a50a61f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindTraders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindTraders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ NorthwindTraders/wwwroot/js/GoogleMapIntegration.js
@@ -0,0 +1,21 @@
+﻿function initMap() {
+    const initialLatLng = { lat: -34.397, lng: 150.644 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: initialLatLng,
+    });
+
+    const marker = new google.maps.Marker({
+        position: initialLatLng,
+        map: map,
+        draggable: true
+    });
+
+    marker.addListener('dragend', function (event) {
+        const coords = `${event.latLng.lat().toFixed(6)}, ${event.latLng.lng().toFixed(6)}`;
+        document.getElementById("geocodedCoordinates").value = coords;
+    });
+}
+
+window.initMap = initMap;
